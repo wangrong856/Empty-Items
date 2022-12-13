2bccee813ebc7f43d620d41e4cca5b5748e84f42
XXXXXXXX <112851799+wangrong856@users.noreply.github.com>
2022-12-13 10:29:00 +0800

    第一次的修订1.1

--- src/components/SvgIcon.vue
+++ src/components/SvgIcon.vue
@@ -1,32 +1,44 @@
 <template>
-    <svg class="svg-icon" aria-hidden="true">
-      <use :xlink:href="iconName"></use>
-    </svg>
-  </template>
-   
-  <script>
-    export default {
-      name: "svg-icon",
-      props: {
-        iconClass: {
-          type: String,
-          required: true,
-        },
-      },
-      computed: {
-        iconName() {
-          return `#icon-${this.iconClass}`
-        },
-      },
-    }
-  </script>
-   
-  <style>
-    .svg-icon {
-      /* 设置图标大小 */
-      width: 2em;
-      height: 2em;
-      /* 用什么颜色来填充图标，如果图标自身有色彩则不再填充 */
-      fill: #000;
-    }
-  </style>
+  <svg class="svg-icon" aria-hidden="true" :style="{ width: w, height: h }">
+    <use :xlink:href="iconName" :style="{ fill: color }"></use>
+  </svg>
+</template>
+
+<script>
+export default {
+  name: "svg-icon",
+  props: {
+    iconClass: {
+      type: String,
+      required: true,
+    },
+    color: {
+      type: String,
+      default: "#000",
+    },
+    w: {
+      type: String,
+      default: "1rem",
+    },
+    h: {
+      type: String,
+      default: "1rem",
+    },
+  },
+  computed: {
+    iconName() {
+      return `#icon-${this.iconClass}`;
+    },
+  },
+};
+</script>
+
+<style>
+.svg-icon {
+  /* 设置图标大小 */
+  /* width: 1rem; */
+  /* height: 1rem; */
+  /* 用什么颜色来填充图标，如果图标自身有色彩则不再填充 */
+  /* fill: #000; */
+}
+</style>
